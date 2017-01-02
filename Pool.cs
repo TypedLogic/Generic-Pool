cedb0e9d46aeea6293a0cd125b6395467a39ace5
Don Oktrova <don.o@live.com>
2017-01-02 11:19:01 -0500

    Initial Commit

---
+++ Pool.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime.Remoting.Messaging;
+
+namespace ResourceManagment
+{
+    public class Pool<T>
+    {
+        /// <summary>
+        /// Number of items in the pool
+        /// </summary>
+        public int Count => _pool.Count;
+
+        /// <summary>
+        /// Boolean whether the pool can return any more objects or not.
+        /// </summary>
+        public bool IsObjectAvailable => _pool.Count == 0 && _createObject == null;
+
+        readonly ConcurrentBag<T> _pool;
+
+        readonly Func<T> _createObject;
+
+        /// <summary>
+        /// Create an empty pool. Object must be put on the pool before they can be taken out.
+        /// </summary>
+        public Pool()
+        {
+            _pool = new ConcurrentBag<T>();
+        }
+
+        /// <summary>
+        /// Create an empty pool. If there are no object available a new object will be created from the function.
+        /// </summary>
+        /// <param name="createObjectFunc">A function to create a new object</param>
+        public Pool(Func<T> createObjectFunc) : this()
+        {
+            _createObject = createObjectFunc;
+        }
+
+        /// <summary>
+        /// Create a pool pre loaded with objects
+        /// </summary>
+        /// <param name="createObjectFunc">A function to create a new object</param>
+        /// <param name="numberofObjectsToPreLoad">The number of objects to pre load</param>
+        public Pool(Func<T> createObjectFunc, int numberofObjectsToPreLoad) : this(createObjectFunc)
+        {
+            for (var i = 0; i < numberofObjectsToPreLoad; i++)
+                Put(createObjectFunc());
+        }
+
+        /// <summary>
+        /// Push an item to the pool
+        /// </summary>
+        /// <param name="item"></param>
+        public void Put(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            _pool.Add(item);
+        }
+
+        /// <summary>
+        /// Pull an item from the pool
+        /// </summary>
+        /// <returns></returns>
+        public T Get()
+        {
+            if (_pool.TryTake(out var item))
+                return item;
+            if (_createObject != null)
+                return _createObject();
+            throw new IndexOutOfRangeException("There are no objects left in the pool and there is no function specified for creating a new object");
+        }
+    }
+}
---
+++ PoolTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ResourceManagment.Tests
+{
+    [TestClass()]
+    public class PoolTests
+    {
+        [TestMethod()]
+        public void PoolTest()
+        {
+            var pool = new Pool<string>();
+            Assert.IsNotNull(pool);
+        }
+
+        [TestMethod()]
+        public void PoolTestWithCreateObjectFunc()
+        {
+            var pool = new Pool<string>(() => "Test");
+            var result = pool.Get();
+
+            Assert.IsNotNull(pool);
+            Assert.AreEqual("Test", result);
+        }
+
+        [TestMethod()]
+        public void PoolTestWithCreateObjectFynctionAndPreload()
+        {
+            var pool = new Pool<string>(() => "Test", 10);
+
+            Assert.IsNotNull(pool);
+            Assert.AreEqual(pool.Count, 10);
+        }
+
+
+        [TestMethod()]
+        public void PutTest()
+        {
+            var pool = new Pool<string>();
+            pool.Put("Test1");
+            pool.Put("Test2");
+
+            Assert.AreEqual(2, pool.Count);
+        }
+
+        [TestMethod(), ExpectedException(typeof(ArgumentNullException))]
+        public void PutTestThrowsException()
+        {
+            var pool = new Pool<string>();
+            pool.Put(null);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var pool = new Pool<string>();
+            pool.Put("Test1");
+            pool.Put("Test2");
+
+            var result = pool.Get();
+            Assert.IsInstanceOfType(result, typeof(string));
+            Assert.AreEqual(1, pool.Count);
+        }
+
+        [TestMethod(), ExpectedException(typeof(IndexOutOfRangeException))]
+        public void GetTestThrowsError()
+        {
+            var pool = new Pool<string>();
+            pool.Get();
+        }
+    }
+}
